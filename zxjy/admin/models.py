743f69862f7036d41e02800a3946545c4822a403
徐先达 <172467548@qq.com>
2019-09-26 07:49:16 +0800

    '测试能否在本地直接修改'

--- zxjy/admin/models.py
+++ zxjy/admin/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Base(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class User(models.Model):
+    pass
+
+
+
