03b570c141f4a0e57493dad1585fc023cfa638dd
徐先达 <172467548@qq.com>
2019-09-26 14:05:21 +0800

    xuxianda

--- zxjy/admin/models.py
+++ zxjy/admin/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class Base(models.Model):
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
